d694b473a4a6d38a2ec3ae057527cb57e8d98cb3
martech <wrkmartech@gmail.com>
2022-06-23 22:10:38 +0300

    Task #2.2 'VueCalculator' solved

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,44 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const rootComponent = defineComponent({
+  data() {
+    return {
+      calcElements: {
+        leftOperand: 0,
+        rightOperand: 0,
+        operator: 'sum',
+      },
+      calcResult: 0,
+    };
+  },
+
+  watch: {
+    calcElements: {
+      handler() {
+        this.calculate();
+      },
+      deep: true,
+    },
+  },
+
+  methods: {
+    calculate() {
+      switch (this.calcElements.operator) {
+        case 'sum':
+          this.calcResult = this.calcElements.leftOperand + this.calcElements.rightOperand;
+          break;
+        case 'subtract':
+          this.calcResult = this.calcElements.leftOperand - this.calcElements.rightOperand;
+          break;
+        case 'multiply':
+          this.calcResult = this.calcElements.leftOperand * this.calcElements.rightOperand;
+          break;
+        case 'divide':
+          this.calcResult = this.calcElements.leftOperand / this.calcElements.rightOperand;
+          break;
+      }
+    },
+  },
+});
+
+createApp(rootComponent).mount('#app');
