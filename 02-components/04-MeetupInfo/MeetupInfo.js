9be9d1db9e4009440c48456f0d9aac4b177f4202
martech <wrkmartech@gmail.com>
2022-06-26 13:58:40 +0300

    Task #3.4 MeetupInfo solved

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -3,19 +3,55 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  methods: {
+    showMeetupDate(type) {
+      if (type === 'locale') {
+        return new Date(this.date).toLocaleDateString(navigator.language, {
+          day: 'numeric',
+          month: 'short',
+          year: 'numeric',
+        });
+      } else {
+        let meetupDate = new Date(this.date);
+        let dateParts = {
+          year: meetupDate.getFullYear(),
+          month: meetupDate.getMonth() < 10 ? '0' + (meetupDate.getMonth() + 1) : meetupDate.getMonth() + 1,
+          day: meetupDate.getDate() < 10 ? '0' + (meetupDate.getDate() + 1) : meetupDate.getDate() + 1,
+        };
+
+        return Object.values(dateParts).join('-');
+      }
+    },
+  },
+
   template: `
     <ul class="meetup-info">
-      <li>
-        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
-      </li>
-      <li>
-        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
-      </li>
-      <li>
-        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
-      </li>
+    <li>
+      <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg"/>
+      {{ organizer }}
+    </li>
+    <li>
+      <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg"/>
+      {{ place }}
+    </li>
+    <li>
+      <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg"/>
+      <time :datetime="showMeetupDate('datetime')">{{ showMeetupDate('locale') }}</time>
+    </li>
     </ul>`,
 });
